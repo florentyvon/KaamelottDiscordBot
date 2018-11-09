629d095c5aec6fb9a8f637ad99c2943c17c8c60b
Julien Raillard <ju.raillard@hotmail.fr>
2018-11-09 16:14:38 +0100

    bullshit

--- commands/citation/random_citation.js
+++ commands/citation/random_citation.js
@@ -2,6 +2,9 @@ const { Command } = require('discord.js-commando');
 const fetch = require('node-fetch');
 
 module.exports = class RandomCitationCommand extends Command{
+   /* init(){
+        this.commands = undefined;
+    }*/
     constructor(client){
         // Only set client + CommandInfo
         super(client, {
@@ -13,31 +16,29 @@ module.exports = class RandomCitationCommand extends Command{
                 'citation (aucun filtre)',
                 'citation -l @numeroDeLivre (filtre sur un livre compris entre 1 et 6)',
                 'citation -a @nomPersonnage (filtre sur le personnage)'
-             ], 
+            ]/*, 
             args: [
                 {
                     key: 'filter',
                     prompt: 'Quel filtre souhaitez-vous? (-l pour livre ou -a pour auteur)',
                     type: 'string',
-                    valide: filter => {
+                    validate: filter => {
                         if (filter === '-l' || filter === '-a' || filter === '') return true;
                         return 'Filtre Erroné';
-                    }
+                    },
+                    default: ""
                 },
                 {
                     key: 'value',
-                    prompt: 'Parmi quel livre souhaitez-vous obtenir une citation?',
-                    type: 'string'
+                    prompt: 'Quel paramètre de filtre souhaitez-vous donner?',
+                    type: 'string',
+                    default: ''                    
                 }
-            ]
+            ]  */          
         }); 
+        //this.commands = {};
     }
-
-    // activated when "!run" is send in channel
-    /*
-    * WARNING : Node support async method but must specify " --harmony " when run the app
-    * so it become : node --harmony . 
-    */
+/*
     async run(message, {filter, value}){  //args are parameter after name command
         
         var api = 'https://kaamelott.chaudie.re/api';
@@ -56,12 +57,90 @@ module.exports = class RandomCitationCommand extends Command{
         
         fetch(api)
             .then(res => res.json())
-            .then(function(json) {
-                message.delete();                
+            .then(function(json) {                
                 message.reply(CitationToString(json));
             })
                 .catch((err) => console.log(err + ' failed ' + filter));
     }
+    */
+   async run(message, args)
+   {     
+       var commands = [];
+       var parameters = [];
+       parameters = args.split('-');
+       console.log(parameters);
+       var length = parameters.length;
+       var cmdFound = false;
+       // create command array
+       for (var i = 1; i < length; i++) {
+            var commandValues = parameters[i].split(' ');
+            console.log("commandValues" + commandValues);
+
+            if(commandValues[0] !== "l" && commandValues[0] !== "a") { console.log(' wrong command'); return;}  // wrong command
+            
+            var commandLength = commands.length;
+            var cmdFound = false;
+            for(var j=0; j<commandLength; j++)
+            {
+                if(commandLength[j].commandName === commandValues[0]) 
+                {
+                    found=true;
+                    break;
+                }
+            }
+
+            if(!cmdFound)
+            {
+                commands.push({commandName: commandValues[0], commandValue: commandValues[1]});
+            }
+            console.log("commands :" + commands);
+            cmdFound=false;            
+        }     
+
+        console.log("before sort" + commands);
+        commands.sort((cmd1,cmd2) => (a.commandName < b.commandName) ? 1 : -1); 
+        console.log("after  sort" +commands);
+
+        fetch(this.GetApiRoute())
+            .then(res => res.json())
+            .then(function(json) {                
+                message.reply(CitationToString(json));
+            })
+                .catch((err) => console.log(err + ' failed ' + filter));
+   }
+
+    AddCommandParameter(cmd, value)
+    {
+        if(this.commands.cmd === undefined)
+        {
+            this.commands.cmd = value;
+        }
+    }
+
+    GetApiRoute()
+    {
+        var apiRoute = 'https://kaamelott.chaudie.re/api';
+
+        if(this.commands.length = 0) return api.concat("/random");
+        
+        apiRoute = api.concat("/random")
+        this.commands.forEach(element => {
+            switch(filter)
+            {
+                case "l": 
+                    apiRoute = api.concat("/livre/" + value);
+                    break;
+                case "a":
+                    apiRoute = api.concat("/personnage/" + value);
+                    break;
+                default:                    
+                    break;
+            }
+        });
+
+        return apiRoute;
+    }   
+
 };
 
 function CitationToString(json)
@@ -71,3 +150,11 @@ function CitationToString(json)
            "\n" + json.citation.infos.saison +
            "\nEpisode : " + json.citation.infos.episode;
 };
+
+function CheckCommandName(cmd)
+{
+    if(cmd === "l" || cmd === "a") return true;
+        return false;
+}
+
+
